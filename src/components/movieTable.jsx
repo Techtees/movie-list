2c2e9116a6559af3be6f96d6845bd4c0728b632f
taiwoj121 <taiwoj121@gmail.com>
2022-08-18 15:44:32 +0100

    created movietable component

---
+++ src/components/movieTable.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Like from './common/like'
+
+
+const MovieTable = props => {
+    const { movies, onDelete, onLike,onSort } = props;
+
+    return ( 
+        <table className="table">
+            <thead>
+                <tr>
+                    <th onClick={ ()=> onSort('title')}>Title</th>
+                    <th onClick={ ()=> onSort('genre.name')}>Genre</th>
+                    <th onClick={ ()=> onSort('numberInStock')}>Stock</th>
+                    <th onClick={ ()=> onSort('dailyRentalRates')}>Rate</th>
+                    <th />
+                    <th />
+                </tr>
+            </thead>
+            <tbody>  
+                {movies.map((movie)=> {
+                    return(
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like liked={movie.liked} onClick={()=> onLike(movie)} />
+                            </td>
+                            <td><button onClick={() => onDelete(movie)} className='btn btn-danger btn-sm'>Delete</button></td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+     );
+}
+ 
+export default MovieTable;
